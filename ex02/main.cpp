218ec3b4ed58a9dac291db7584f009e4492fb350
Lukas Seeger <lseeger@.42heilbronn.de>
2025-11-20 15:00:09 +0100

    more sophisticated tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,6 +12,31 @@ int main(void) {
             std::cout << arr[i] << " ";
         }
         std::cout << std::endl;
+
+        std::cout << "Test assignment operator" << std::endl;
+        Array<int> arr2(20);
+        arr2 = arr;
+        arr2[10] = 100;
+        for (size_t i = 0; i < arr.size(); i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+        for (size_t i = 0; i < arr2.size(); i++) {
+            std::cout << arr2[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Test copy constructor" << std::endl;
+        Array<int> arr3(arr2);
+        arr3[10] = 200;
+        for (size_t i = 0; i < arr2.size(); i++) {
+            std::cout << arr2[i] << " ";
+        }
+        std::cout << std::endl;
+        for (size_t i = 0; i < arr3.size(); i++) {
+            std::cout << arr3[i] << " ";
+        }
+        std::cout << std::endl;
     }
 
     // Basic float Array
@@ -36,5 +61,11 @@ int main(void) {
             std::cout << arr[i] << " ";
         }
         std::cout << std::endl;
+        std::cout << "Test wrong Access" << std::endl;
+        try {
+            std::cout << arr[200] << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 }
